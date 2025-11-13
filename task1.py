b1cb682de9d870c4241df5740dcec845a3930789
Soham Jain <95491523+SohamJain2007@users.noreply.github.com>
2025-11-13 18:55:45 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+f1 = int(input("Enter first number: "))
+f2 = int(input("Enter second number: "))
+
+Addition = f1 + f2 
+Subtraction = f1 - f2
+Multiplicattion = f1*f2
+Divison = f1/f2
+print(f"Additon: {Addition}")
+print(f"Subtraction: {Subtraction}")
+print(f"Multiplication: {Multiplicattion}")
+print(f"Division: {Divison}")
